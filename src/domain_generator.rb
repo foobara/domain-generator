0af58418a0e6b189debbcc82c9a984862ae1d30a
Miles Georgi <azimux@gmail.com>
2024-03-09 20:13:19 -0800

    Remove unecessary overrides

--- src/domain_generator.rb
+++ src/domain_generator.rb
@@ -35,17 +35,6 @@ module Foobara
           def templates_dir
             "#{__dir__}/../templates"
           end
-
-          # TODO: promote this up to base project
-          def ==(other)
-            # :nocov:
-            self.class == other.class && domain_config == other.domain_config
-            # :nocov:
-          end
-
-          def hash
-            domain_config.hash
-          end
         end
       end
     end
